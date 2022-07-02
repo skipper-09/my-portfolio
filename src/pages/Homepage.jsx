37082e0706a343158ff986b8001881e1a776d30c
skipper-09 <dxdskipper@gmail.com>
2022-07-03 06:43:06 +0700

    coba

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ export default function Homepage(props) {
                     <div className="row">
                         <div className="col-6">
                             <h2 className='text-capitalize '>Ahmad Nurul Musthopa</h2>
-                            <p>Saya seorang mahasiswa semester 6 di stikom pgri banyuwangi </p>
+                            <p>Saya Saat ini seorang freelancer</p>
                             <div className="d-flex gap-2">
                                 <button className="btn btn-primary hover btn-lg ">Here Me</button>
                                 <button className="btn btn-secondary ">Job Me</button>
