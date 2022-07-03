477baf983b327e268c92753183dafa8d3887be4c
skipper-09 <dxdskipper@gmail.com>
2022-07-03 21:55:30 +0700

    add

--- src/component/footer.js
+++ src/component/footer.js
@@ -3,11 +3,12 @@ import Logo from 'assets/images/logov2.png';
 export default function Footer() {
     return (
         <>
-            <footer className="p-4">
+            <footer className=" bg-light">
                 <div className="container">
                     <div className="row ">
-                        <div className="col-3">
-                            <img src={Logo} alt="logo" width={100} />
+                        <div className="col-md-6">
+                            <h4>Dapatkan info Tengtang Ku</h4>
+                            <p>Untuk Bisnis dapat menghubungi email <a href='mailto:musthopaa043@gmail.com'>musthopaa043@gmail</a></p>
                         </div>
                     </div>
                 </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
-export default function Homepage(props) {
+export default function Homepage() {
     return (
         <>
             <section>
                 <div className='container'>
                     <div className="row">
-                        <div className="col-6">
-                            <h2 className='text-capitalize '>Ahmad Nurul Musthopa</h2>
-                            <p>Saya seorang mahasiswa semester  </p>
+                        <div className="col-md-6 col-12 col-lg-6">
+                            <h2 className='text-capitalize '>Hi I'm Ahmad Nurul Musthopa</h2>
+                            <p>Saya seorang mahasiswa di Sekolah Ilmu Komputer di Banyuwangi. saya seorang freelancer  </p>
                             <div className="d-flex gap-2">
                                 <button className="btn btn-primary hover btn-lg ">Here Me</button>
                                 <button className="btn btn-secondary ">Job Me</button>
                             </div>
                         </div>
+
+                        <div className='col-md-6 col-12 col-lg-6'>
+                            <img src="" alt="" />
+                        </div>
                     </div>
                 </div>
             </section>
-
         </>
     )
 }
