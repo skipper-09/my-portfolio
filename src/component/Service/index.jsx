c60b5d41f17b76b7f4024c990d30719855b9ccdd
skipper-09 <dxdskipper@gmail.com>
2022-07-06 13:05:09 +0700

    fix service

--- src/component/Service/index.jsx
+++ src/component/Service/index.jsx
@@ -1,51 +1,25 @@
 import React from 'react'
 import ImgService from 'assets/images/service.svg'
-import Developer from 'assets/images/developer.svg'
+import ServiceCard from 'element/service-card'
+import { ServiceData } from 'data/data'
 export default function Service() {
   return (
     <>
       <section className=''>
         <div className='container'>
 
-          <div className="row">
-            <div className="col">
+          <div className="row align-items-center">
+            <div className="col-md-6">
               <img src={ImgService} alt="Service Image" className='img-fluid' />
             </div>
-            <div className="col">
-              <div className='title'><h2 className='display-6 fw-bold'>My Servcice</h2></div>
-              <div className='box rounded'>
-                <div className='row p-3'>
-                  <div className="col-12 gap-1 ">
-                    <div className='d-flex box-child  align-items-center gap-3 p-3 rounded '>
-                      <img src={Developer} alt="developer" className='img-fluid rounded-circle' width={60} />
-                      <div className='row'>
-                        <div className="col"><span className='fw-bold'>Developer</span></div>
-                        <span>Keahlihan Programing saya tidak diragukan lagi</span>
-                        <div className="col"></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-12 mt-3 ">
-                    <div className='d-flex box-child  align-items-center gap-3 p-3 rounded '>
-                      <img src={Developer} alt="developer" className='img-fluid rounded-circle' width={60} />
-                      <div className='row'>
-                        <div className="col"><span className='fw-bold'>Developer</span></div>
-                        <span>Keahlihan Programing saya tidak diragukan lagi</span>
-                        <div className="col"></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-12 mt-3 ">
-                    <div className='d-flex box-child  align-items-center gap-3 p-3 rounded '>
-                      <img src={Developer} alt="developer" className='img-fluid rounded-circle' width={60} />
-                      <div className='row'>
-                        <div className="col"><span className='fw-bold'>Developer</span></div>
-                        <span>Keahlihan Programing saya tidak diragukan lagi</span>
-                        <div className="col"></div>
-                      </div>
-                    </div>
-                  </div>
-
+            <div className="col-md-6 ">
+              <h2 className='display-6 fw-bold subtitle'>My Service</h2>
+              <div className='mt-3 p-4 box rounded shadow'>
+                <div className='row gy-4 '>
+                  {ServiceData.map((data) => (
+                    <ServiceCard image={data.image} name_service={data.title} service_details={data.deskription} />
+                  )
+                  )}
                 </div>
               </div>
             </div>
